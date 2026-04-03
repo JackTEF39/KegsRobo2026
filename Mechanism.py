bd409cc64e678d44f3b736e870962002af3f051e
neso17 <nessofi612@gmail.com>
2026-04-04 00:44:48 +0100

    Refactor mechanism functions and update servo positions

--- Mechanism.py
+++ Mechanism.py
@@ -1,14 +1,15 @@
 from sr.robot3 import Colour, LED_A, LED_B, LED_C
 
+from Helpers import *
 from Movement import *
 
 def mechanismOpen(robot, servo1, servo2):
-    servo1.position = -0
-    servo2.position = 0
+    servo1.position = 0
+    servo2.position = 1 
 
 def mechanismClose(robot, servo1, servo2):
     servo1.position = 1
-    servo2.position = -1   
+    servo2.position = 0  
 
 def mechanismTest(robot, servo1, servo2): #currently 0 is open, 1 is closed
     mechanismOpen(robot, servo1, servo2)
@@ -16,24 +17,15 @@ def mechanismTest(robot, servo1, servo2): #currently 0 is open, 1 is closed
     mechanismClose(robot, servo1, servo2)
     robot.sleep(1)
 
-def indicatePowerOn(robot):
-    robot.kch.leds[LED_A].colour = Colour.RED
-    robot.kch.leds[LED_B].colour = Colour.BLUE
-    robot.kch.leds[LED_C].colour = Colour.GREEN
-    robot.sleep(3)
-    robot.kch.leds[LED_A].colour = Colour.OFF
-    robot.kch.leds[LED_B].colour = Colour.OFF
-    robot.kch.leds[LED_C].colour = Colour.OFF
-
-def sweep(robot, servo):
+def unsweep(robot, servo):
     servo.position = 1
     robot.sleep(0.5)
     stepMotorsRotate(robot, 90)
-    servo.position = 0
+    servo.position = -0.5
     robot.sleep(0.5)
 
-def unsweep(robot, servo):
-    servo.position = 0
+def sweep(robot, servo):
+    servo.position = -0.5
     robot.sleep(0.5)
     servo.position = 1
     robot.sleep(0.5)
@@ -42,13 +34,13 @@ def deposit(robot, servo1, servo2):
     print("Depositing")
     mechanismOpen(robot, servo1, servo2)
     robot.sleep(1)
-    stepMotors(robot, stepMotors(robot, 200)) # Move back to deposit 
+    stepMotors(robot, 200) # Move back to deposit 
     mechanismClose(robot, servo1, servo2)
     stepMotorsRotate(robot,60)
 
-def sweeper(robot, servo1, servo2):
+def sweeper(robot, servo1):
     print("Sweeping")
-    stepMotors(robot, stepMotors(robot, 200)) # Move forward to sweep
+    stepMotors(robot, 200) # Move forward to sweep
     sweep(robot, servo1)
-    robot.sleep(1)
-    stepMotors(robot, convertDistToSteps(robot, -200)) # Move back after sweeping
+    robot.sleep(3)
+    stepMotors(robot, -200) # Move back after sweeping
